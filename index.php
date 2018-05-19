b8cacaca92319569d467d1750cda238344495a8b
Arakkal Fasil <farakkal@woolworths.com.au>
2018-05-20 09:14:48 +1000

    Updated contact details

--- index.php
+++ index.php
@@ -52,91 +52,7 @@
 </head>
 
 <body class="page-parent template-slider color-blue layout-full-width header-classic subheader-transparent sticky-header sticky-white subheader-title-left">
-    <!-- Hidden Top Area -->
-    <div id="Sliding-top">
-        <div class="widgets_wrapper">
-            <div class="container">
-                <div class="one-fourth column">
-                    <!-- Login form -->
-                    <aside id="widget_mfn_login-2" class="widget widget_mfn_login">
-                        <div class="mfn-login">
-                            <h4>Login | Register</h4>
-                            <form name="loginform" id="loginform" action="#" method="post">
-                                <p class="login-username">
-                                    <label for="user_login">Username</label>
-                                    <input type="text" name="log" id="user_login" class="input" value="" size="20" />
-                                </p>
-                                <p class="login-password">
-                                    <label for="user_pass">Password</label>
-                                    <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" />
-                                </p>
-                                <p class="login-submit">
-                                    <input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Log In" />
-                                    <input type="hidden" name="redirect_to" value="#" />
-                                </p>
-                            </form>
-                            <div class="links">
-                                <a href="#">Register</a><span class="sep">|</span><a href="#">Lost your password?</a>
-                            </div>
-                        </div>
-                    </aside>
-                </div>
-                <div class="one-fourth column">
-                    <!-- Text Area -->
-                    <aside id="text-10" class="widget widget_text">
-                        <div class="textwidget">
-                            <p>
-                                <span class="big">We reply on all questions within</span>
-                            </p><span class="themecolor flv_header_number">24 h</span>
-                            <div class="hr_dots flv_dots">
-                                <span></span><span></span><span></span>
-                            </div>
-                            <p class="flv_center">
-                                <span class="flv_grey">We offer support for our customers</span>
-                                <br>
-                                <b><i class="icon-clock"></i> Mon - Fri 8:00am - 6:00pm <span class="tooltip" data-tooltip="we are located in Europe" >(GMT +1)</span> </b>
-                            </p>
-                        </div>
-                    </aside>
-                </div>
-                <div class="one-fourth column">
-                    <!-- Text Area -->
-                    <aside id="text-11" class="widget widget_text">
-                        <div class="textwidget">
-                            <h5 class="flv_margin_b_5"><i class="icon-mail-line"></i> Our address:</h5>
-                            <p class="flv_margin_l_26">
-                                <strong>Envato</strong>
-                                <br> Level 13, 2 Elizabeth St,
-                                <br> Melbourne, Victoria 3000 Australia
-                            </p>
-                            <h5 class="flv_margin_b_5"><i class="icon-phone"></i> Have any questions?</h5>
-                            <p class="flv_margin_l_26">
-                                <a href="mailto:noreply@envato.com">noreply@envato.com</a>
-                            </p>
-                            <h5 class="flv_margin_b_5"><i class="icon-comment-line"></i> Call us:</h5>
-                            <p class="flv_margin_l_26">
-                                <a href="tel:+61791803458">+61 (0) 7 9180 3458</a>
-                            </p>
-                        </div>
-                    </aside>
-                </div>
-                <div class="one-fourth column">
-                    <!-- Text Area -->
-                    <aside id="text-12" class="widget widget_text">
-                        <div class="textwidget"><img src="images/logo_footer.png" alt="">
-                            <p>
-                                <span class="big">We love who we are and we are very proud to be the part of your business</span>
-                            </p>
-                            <p>
-                                Curabitur sit amet magna quam. Praesent in libero vel <span class="tooltip" data-tooltip="Quis accumsan dolor">turpis pellentesque</span> egestas sit amet vel nunc. Nunc lobortis dui neque quis.
-                            </p>
-                        </div>
-                    </aside>
-                </div>
-            </div>
-        </div>
-        <a href="#" class="sliding-top-control"><span><i class="plus icon-down-open-mini"></i><i class="minus icon-up-open-mini"></i></span></a>
-    </div>
+   
     <!-- Main Theme Wrapper -->
     <div id="Wrapper">
         <!-- Header Wrapper -->
@@ -153,40 +69,19 @@
                                     Have any questions?
                                 </li>
                                 <li class="phone">
-                                    <i class="icon-phone"></i><a href="tel:+61383766284">+61 383 766 284</a>
+                                    <i class="icon-phone"></i><a href="tel:00914842655040">00914842655040</a>
                                 </li>
                                 <li class="mail">
-                                    <i class="icon-mail-line"></i><a href="mailto:noreply@envato.com">noreply@envato.com</a>
+                                    <i class="icon-mail-line"></i><a href="mailto:info@highnessindia.com">info@highnessindia.com</a>
                                 </li>
                             </ul>
                             <!--Social info area-->
                             <ul class="social">
-                                <li class="skype">
-                                    <a href="#" title="Skype"><i class="icon-skype"></i></a>
-                                </li>
                                 <li class="facebook">
-                                    <a href="https://www.facebook.com/Beantown-Themes-653197714728193" title="Facebook"><i class="icon-facebook"></i></a>
-                                </li>
-                                <li class="googleplus">
-                                    <a href="https://plus.google.com/" title="Google+"><i class="icon-gplus"></i></a>
+                                    <a href="https://www.facebook.com/Highness-Doors-1817969171802557/" title="Facebook"><i class="icon-facebook"></i></a>
                                 </li>
                                 <li class="twitter">
-                                    <a href="#" title="Twitter"><i class="icon-twitter"></i></a>
-                                </li>
-                                <li class="vimeo">
-                                    <a href="https://vimeo.com/" title="Vimeo"><i class="icon-vimeo"></i></a>
-                                </li>
-                                <li class="youtube">
-                                    <a href="#" title="YouTube"><i class="icon-play"></i></a>
-                                </li>
-                                <li class="flickr">
-                                    <a href="https://www.flickr.com/" title="Flickr"><i class="icon-flickr"></i></a>
-                                </li>
-                                <li class="pinterest">
-                                    <a href="https://www.pinterest.com/" title="Pinterest"><i class="icon-pinterest"></i></a>
-                                </li>
-                                <li class="dribbble">
-                                    <a href="https://dribbble.com" title="Dribbble"><i class="icon-dribbble"></i></a>
+                                    <a href="https://twitter.com/highnessdoors" title="Twitter"><i class="icon-twitter"></i></a>
                                 </li>
                             </ul>
                         </div>
@@ -721,31 +616,7 @@
                                     </form>
                                 </div>
                             </div>
-                            <div class="top_bar_right">
-                                <div class="top_bar_right_wrapper">
-                                    <!-- Shopping cart icon-->
-                                    <a id="header_cart" href="#"><i class="icon-basket"></i><span>8</span></a><a id="search_button" href="#"><i class="icon-search"></i></a>
-                                    <!--wpml flags selector-->
-                                    <div class="wpml-languages enabled">
-                                        <a class="active tooltip" href="#" data-tooltip="No translations available for this page"><img src="images/flags/en.png" alt="English" /><i class="icon-down-open-mini"></i>
-                                        </a>
-                                        <ul class="wpml-lang-dropdown">
-                                            <li>
-                                                <a href="#"><img src="images/flags/fr.png" alt="French" />
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#"><img src="images/flags/de.png" alt="German" />
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#"><img src="images/flags/pl.png" alt="Polish" />
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
